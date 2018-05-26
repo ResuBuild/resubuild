d9b8f92db86ab77ba7c6b2a89735cef83b0d1001
AlexVuc <alex.vucicevich@ppo.ca>
2018-05-26 13:49:01 -0400

    App.js changes
    Mongo mlab changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var LocalStrategy = require('passport-local').Strategy;
 const { matchedData, sanitize } = require('express-validator/filter');
 const port = 3000;
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1/resuBuild",function(err) {
+mongoose.connect("mongodb://"+process.env.dbUsername":"+process.env.dbPassword+"@ds135760.mlab.com:35760/resubuild",function(err) {
     if (err)
         errorDB = true;
 });
